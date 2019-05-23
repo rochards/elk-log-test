08702985adc72992b00d553c6cf41aa82c04c573
Rodrigo Rocha <rodrigorochasantos@outlook.com>
2019-05-23 15:21:37 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Aplicação criada para geração de logs simples.
+Os logs são gerados no diretório /tmp/elk-log-test.
---
+++ src/main/java/br/com/rochards/main/App.java
@@ -0,0 +1,30 @@
+package br.com.rochards.main;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	final static Logger logger = Logger.getLogger(App.class);
+	
+    public static void main( String[] args )
+    {
+        while(true) {
+        	
+        	logger.info("log test info");
+            logger.warn("log test warn");
+            logger.debug("log test debug");
+        	
+        	try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				logger.error(e.getMessage());
+			}
+        }
+        
+    }
+}
