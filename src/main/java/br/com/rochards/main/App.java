d7fb9bbc0b38605914920fcc301cf44827f7e277
Rodrigo Rocha dos Santos <43471650+rochards@users.noreply.github.com>
2020-06-23 10:18:18 -0300

    Update App.java
    Indentando o código.

--- src/main/java/br/com/rochards/main/App.java
+++ src/main/java/br/com/rochards/main/App.java
@@ -6,25 +6,24 @@ import org.apache.log4j.Logger;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
 	final static Logger logger = Logger.getLogger(App.class);
 	
-    public static void main( String[] args )
-    {
-        while(true) {
+	public static void main( String[] args ) {
+        
+		while(true) {
         	
-        	logger.info("log test info");
-            logger.warn("log test warn");
-            logger.debug("log test debug");
+        		logger.info("log test info");
+            		logger.warn("log test warn");
+            		logger.debug("log test debug");
         	
-        	try {
+        		try {
 				Thread.sleep(100);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				logger.error(e.getMessage());
 			}
-        }
+        	}
         
-    }
+    	}
 }
